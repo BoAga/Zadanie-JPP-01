0661c08591474837b15ad0cd9af233f0e6d1d13c
Aga <aga@git.pl>
2020-02-22 15:20:46 +0100

    Personal data added

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Zadanie1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Write your name ");
+
+        String surname = sc.nextLine();
+        System.out.println("Write yout surname ");
+
         
+
+
     }
 }
